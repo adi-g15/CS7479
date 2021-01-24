4887eebe7b404e603da71d375056b3e5c58302e9
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-01-25 00:40:39 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,5 +4,5 @@ import "../styles/global.css";
 export default ContentPage.bind(this, {
 	title: "Decrypted CS4401",
 	dirPath: "cs4401/",
-	msg: "Decrypted Lecture Notes CS4401 :D, no password"
+	msg: "Decrypted Lecture Notes CS4401, bina password  :D  {Updated: 25th Jan}"
 });

65d07570be6a5b1c9f27a70814e68341aeac6121
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-03-18 14:23:52 +0530

    build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,93 @@
-import ContentPage from "../components/ContentPage";
+import React, { useEffect, useState } from 'react';
+import NavBar from "../components/navbar";
+import PDFDown from "../components/pdf_down";
+import byteSize from "byte-size";
+import {GetListService} from "../services/files";
 import "../styles/global.css";
 
-export default ContentPage.bind(this, {
-	title: "Decrypted CS4401",
-	dirPath: "cs4401/",
-	msg: "Decrypted Lecture Notes CS4401, bina password  :D  {Updated: 25th Jan}"
-});
+import firebase from "firebase/app";
+import firebaseConfig from "../config/firebase";
+import "firebase/storage";
+
+try{
+	firebase.initializeApp(firebaseConfig);
+} catch(err) {
+	console.error(err.msg || "Error");
+}
+
+const storage = firebase.storage();
+const storageRef = storage.ref();
+
+export default function CS4401() {
+	const [files, setFiles] = useState([]);
+	const [zipLink, setZipLink] = useState('?');
+	const [zipSize, setZipSize] = useState(0);
+	const [selectOn, setSelect] = useState(false);
+
+	useEffect(() => {
+		GetListService(storageRef.child("cs4401/")).then(data => {
+			console.debug(data);
+
+			setFiles(data.storedFiles);
+			if(data.zipped) {
+				data.zipped.link.then(link => setZipLink(link));
+				data.zipped.meta.then(metadata => setZipSize(metadata.size));
+			}
+		})
+	}, []);
+
+	// <></> is a react fragement
+	return (
+		<>
+			<NavBar title="Decrypted CS4401"/>
+
+			<hr className="separation" />
+			<div className="centered">
+				{"Decrypted Lecture Notes CS4401, bina password  :D"}
+			</div>
+			<hr className="separation" />
+			<br />
+			<div className="link_container">
+			<div><button
+				style={{
+					backgroundColor: '#20b2aa30',
+					width: '80%',
+					marginLeft: '10%',
+					borderRadius: '10px'
+				}}
+				onClick={() => document.getElementById("zip_anchor").click()}
+				disabled={zipLink === '?'}
+			>
+				<a download id="zip_anchor" href={zipLink} style={{color: 'inherit', textDecoration: 'none'}}>
+					Download All as ZIP
+				</a>
+				<p>
+					{ `( ${byteSize(zipSize).toString()} )`}
+				</p>
+			</button></div>
+
+				<table>
+					<thead>
+						<tr>
+							{selectOn && (<td>Select</td>)}
+							<td>Name</td>
+							<td>Size</td>
+						</tr>
+					</thead>
+					<tbody>
+					{files.map((value, index) => (
+						<PDFDown
+							name={value.name}
+							linkPromise={value.link}
+							metaPromise={value.meta}
+							key={index}
+							selectOn={selectOn}
+						/>
+					)
+					)}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
+}
